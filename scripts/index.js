9c1eb707975110531df676e1709afbcecd8b564d
Kseniya Zemlyanko <shikaito@yandex.ru>
2022-01-09 15:26:50 +0300

    доработка переключения темы

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const checkbox = document.querySelector(".checkbox__input");
+const checkbox = document.querySelectorAll(".checkbox__input");
 let theme = "light";
 
 const bikesHighway = [
@@ -157,30 +157,37 @@ btnPlain.addEventListener("click", () => {
   });
 });
 
-checkbox.addEventListener("click", () => {
-  if (theme === "light") {
-    document.documentElement.style.setProperty("--color-bg-base", "#333");
-    document.documentElement.style.setProperty("--color-dark", "#fff");
-    document.documentElement.style.setProperty("--color-light", "#e5e5e5");
-    document.documentElement.style.setProperty("--color-line", "#707070");
-    document.documentElement.style.setProperty("--color-bg-checkbox","#545454");
-    document.documentElement.style.setProperty("--color-bg-footer", "#2f2f2f");
-    document.documentElement.style.setProperty("--color-copyright", "#565656");
-    document.querySelector("#back").style.backgroundImage = "url('./images/back-dark.svg')";
-    document.querySelector("#next").style.backgroundImage = "url('./images/next-dark.svg')";
-    document.querySelector(".checkbox__ball").style.left = "23px";
-    theme = "dark";
-  } else {
-    document.documentElement.style.setProperty("--color-bg-base", "#f4f4f4");
-    document.documentElement.style.setProperty("--color-dark", "#151515");
-    document.documentElement.style.setProperty("--color-light", "#222222");
-    document.documentElement.style.setProperty("--color-line", "#d7d4d4");
-    document.documentElement.style.setProperty("--color-bg-checkbox", "#fff");
-    document.documentElement.style.setProperty("--color-bg-footer", "#efefef");
-    document.documentElement.style.setProperty("--color-copyright", "#cfcfcf");
-    document.querySelector("#back").style.backgroundImage = "url('./images/back.svg')";
-    document.querySelector("#next").style.backgroundImage = "url('./images/next.svg')";
-    document.querySelector(".checkbox__ball").style.left = "0";
-    theme = "light";
-  }
+checkbox.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    if (theme === "light") {
+      document.documentElement.style.setProperty("--color-bg-base", "#333");
+      document.documentElement.style.setProperty("--color-dark", "#fff");
+      document.documentElement.style.setProperty("--color-light", "#e5e5e5");
+      document.documentElement.style.setProperty("--color-line", "#707070");
+      document.documentElement.style.setProperty("--color-bg-checkbox","#545454");
+      document.documentElement.style.setProperty("--color-bg-footer", "#2f2f2f");
+      document.documentElement.style.setProperty("--color-copyright", "#565656");
+      document.querySelector("#back").style.backgroundImage = "url('./images/back-dark.svg')";
+      document.querySelector("#next").style.backgroundImage = "url('./images/next-dark.svg')";
+      document.querySelectorAll(".checkbox__ball").forEach((elem) => {
+        elem.style.left = "23px";
+      });
+      theme = "dark";
+    } else {
+      document.documentElement.style.setProperty("--color-bg-base", "#f4f4f4");
+      document.documentElement.style.setProperty("--color-dark", "#151515");
+      document.documentElement.style.setProperty("--color-light", "#222222");
+      document.documentElement.style.setProperty("--color-line", "#d7d4d4");
+      document.documentElement.style.setProperty("--color-bg-checkbox", "#fff");
+      document.documentElement.style.setProperty("--color-bg-footer", "#efefef");
+      document.documentElement.style.setProperty("--color-copyright", "#cfcfcf");
+      document.querySelector("#back").style.backgroundImage = "url('./images/back.svg')";
+      document.querySelector("#next").style.backgroundImage = "url('./images/next.svg')";
+      document.querySelectorAll(".checkbox__ball").forEach((elem) => {
+        elem.style.left = "0";
+      });
+      theme = "light";
+    }
+  });
 });
+
